1d0c55905c32ae1fd6036caee56401dc9ebbd74b
Michał Żak <michal.zak000@gmail.com>
2023-12-21 14:11:02 +0100

    Add new product page and responsive CSS

--- frontpage_app/views.py
+++ frontpage_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontpage_app/index.html')
+
+def product(request):
+    return render(request, 'frontpage_app/product.html')
