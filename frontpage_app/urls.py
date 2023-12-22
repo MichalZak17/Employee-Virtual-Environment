9e94205282af38808245a4091fc0a972d67489a3
Michał Żak <michal.zak000@gmail.com>
2023-12-22 01:10:03 +0100

    Add new URLs for use cases and contact us pages

--- frontpage_app/urls.py
+++ frontpage_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/', views.product, name='product'),
+    path('use-cases/', views.use_cases, name='use_cases'),
+    path('contact-us/', views.contact_us, name='contact_us'),
 ]
--- frontpage_app/views.py
+++ frontpage_app/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def product(request):
     return render(request, 'frontpage_app/product.html')
+
+def use_cases(request):
+    return render(request, 'frontpage_app/use_cases.html')
+
+def contact_us(request):
+    return render(request, 'frontpage_app/contact_us.html')
