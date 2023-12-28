5b3a63fe5a9901ca74923067bb28b9686bf9bbeb
Michał Żak <michal.zak000@gmail.com>
2023-12-28 12:16:35 +0100

    Refactor views.py file
    This commit refactors the views.py file in the frontend directory. It includes the following changes:
    - Added the import statement for the `login_required` decorator from `django.contrib.auth.decorators`.
    - Created a new decorator `team_leader_required` that checks if the user is authenticated and if they are a team lead.
    - Added the `@login_required` and `@team_leader_required` decorators to the `dashboard` view function.
    - Fixed the formatting of the existing code by using consistent quotation marks.

--- frontend/views.py
+++ frontend/views.py
@@ -1,27 +1,48 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 def redirect_authenticated_user(view_func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('admin/dashboard/')
+            return redirect("admin/dashboard/")
         return view_func(request, *args, **kwargs)
+
     return wrapper
+
 
 @redirect_authenticated_user
 def index(request):
-    return render(request, 'frontend/home.html')
+    return render(request, "frontend/home.html")
+
 
 @redirect_authenticated_user
 def product(request):
-    return render(request, 'frontend/product.html')
+    return render(request, "frontend/product.html")
+
 
 @redirect_authenticated_user
 def use_cases(request):
-    return render(request, 'frontend/use_cases.html')
+    return render(request, "frontend/use_cases.html")
+
 
 @redirect_authenticated_user
 def contact_us(request):
-    return render(request, 'frontend/contact_us.html')
+    return render(request, "frontend/contact_us.html")
 
+
+def team_leader_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect("login")
+        elif not request.user.user_is_team_lead:
+            return redirect("/login/")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+@login_required(login_url="/login/")
+@team_leader_required
 def dashboard(request):
-    return render(request, 'frontend/dashboard.html')
+    return render(request, "frontend/dashboard.html")
